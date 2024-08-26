e46c5698507382939991eb6e5330ee102168d73f
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-26 23:02:38 +0100

    Add files via upload

--- tipos_variaveis/texto.py
+++ tipos_variaveis/texto.py
@@ -13,3 +13,4 @@ sobrenome = "Braun"
 # Formatação
 print("nome_completo(1° forma):", nome_completo)
 print("nome_completo(2° forma):" + nome_completo)
+print("nome_completo(3° forma):" + nome_completo + "Braun")
