de6e864d9e4f61b24ee572c575dded38e75c0d00
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-26 23:07:24 +0100

    Add files via upload

--- tipos_variaveis/texto.py
+++ tipos_variaveis/texto.py
@@ -4,7 +4,7 @@ nome_completo = "Paulo Braun"
 nome_completo_aspas = """Paulo 
 Braun"""
 
-numero_completo_quebra = "Paulo \
+nome_completo_quebra = "Paulo \
 Braun"
 
 nome = "Paulo"
@@ -15,3 +15,5 @@ print("nome_completo(1° forma):", nome_completo)
 print("nome_completo(2° forma):" + nome_completo)
 print("nome_completo(3° forma):" + "Paulo" + "Braun")
 print("nome_completo(4° forma):" + "Paulo" , "Braun")
+print("nome_completo(5° forma):", nome_completo_aspas)
+print("nome_completo(6° forma):", nome_completo_quebra)
