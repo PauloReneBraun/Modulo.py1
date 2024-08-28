c8961f93038c526569e0bc444d01f25dfbefc576
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-28 18:57:42 +0100

    Add files via upload

--- tipos_variaveis/lista.py
+++ tipos_variaveis/lista.py
@@ -5,6 +5,7 @@ minha_lista = [1, 2, 3, 4, 5, "OW", True, False]
 print("Minha lista de exemplo", minha_lista)
 
 # Exibindo a lista 
+minha_lista[0] = "Python"
 print("minha lista[0]:",minha_lista[0])
 print("minha lista[5]:",minha_lista[5])
 print("minha lista[1:7]:",minha_lista[1:7])
