93fb1e0781e47354e0cd0f7cd6687e8b0a56d000
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-28 21:09:37 +0100

    Add files via upload

--- tipos_variaveis/tupla.py
+++ tipos_variaveis/tupla.py
@@ -3,4 +3,6 @@ minha_tupla = (1,2,3,4)
 
 print("Minha tupla:", minha_tupla)
 
-print("Minha tupla:", minha_tupla[0])
+print("Minha tupla[0]:", minha_tupla[0])
+print("Minha tupla[0]:", minha_tupla[2])
+print("Minha tupla[0]:", minha_tupla[-1])
