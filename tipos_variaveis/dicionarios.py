0e21dd13d5c2482207fdbd9d7c7d6138773235b8
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-28 22:52:29 +0100

    Add files via upload

--- tipos_variaveis/dicionarios.py
+++ tipos_variaveis/dicionarios.py
@@ -1,2 +1,7 @@
 #Criando um dicionário de exemplo 
-pessoa = {"nome": "Mateus", "idade":20, "cidade": "Curitiba"}
+pessoa = {"nome": "Mateus", "idade": 20, "cidade": "Curitiba"}
+
+#exibindo o dicionario 
+print("Meu dicionário de exemplo:", pessoa)
+
+# Acessando valor 
