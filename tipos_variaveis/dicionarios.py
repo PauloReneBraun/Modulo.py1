7764a8681512b1afca6a00362c63e2456e258a9d
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-30 16:55:11 +0100

    Add files via upload

--- tipos_variaveis/dicionarios.py
+++ tipos_variaveis/dicionarios.py
@@ -18,3 +18,8 @@ print("Idade atualizada:", pessoa["idade"])
 # Removendo um par
 del pessoa["sobrenome"]
 print("Meu dicionário de exemplo:", pessoa)
+
+#Método: keys(), values(), items()
+chaves = pessoa.keys()
+print("Chaves do dicionário:", chaves)
+print("Pirmeira chave:", chaves[0])
