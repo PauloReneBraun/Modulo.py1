82474e2a45a1c181926dadca4ed1bae85042b255
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-02 14:08:02 +0100

    Add files via upload

--- tipos_variaveis/condicionais.py
+++ tipos_variaveis/condicionais.py
@@ -5,6 +5,8 @@ idade = 18
 print("Exemplo de comando if")
 if idade >= 18:
   print("Você é maior de idade.")
+elif idade >= 12:
+  print("Você é um adolecente")
 else:
   print("Você é menor de idade")
 
