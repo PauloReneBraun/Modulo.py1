c7930ec9de23eef48fadac907b76fcbd23c18839
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-02 18:10:34 +0100

    Add files via upload

--- tipos_variaveis/for.py
+++ tipos_variaveis/for.py
@@ -1,3 +1,9 @@
+print("For utilizando lista")
 lista = [1, 2, 3, 4, 5]
 for elementor in lista:
   print(elementor)
+
+print("For utilizando tupla")
+tupla = (2, 3, 4, 5, 6)
+for elemento in tupla:
+    print(elemento)
