f878be9fe8426e76e6ca50f2bfe6a8466700f934
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-03 22:28:49 +0100

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -17,3 +17,7 @@ print("Resultado da funcao quadrado:", resultado_quadrado)
 def soma(numero1, numero2):
   resultado = numero1 + numero2
   return resultado
+
+print("\n Chamando a função soma:")
+resultado_soma = soma(20, 50)
+print("A soma do numero 20 e numero 50 é", resultado_soma)
