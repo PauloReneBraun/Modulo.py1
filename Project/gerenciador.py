054b9b0c9a423f9d26b1d36c5e4d731b0842acce
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-16 19:33:16 +0100

    Add files via upload

--- Project/gerenciador.py
+++ Project/gerenciador.py
@@ -14,7 +14,7 @@ def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome_tarefa):
     print("Tarefa não encontrada")
     return
   indice_tarefa_ajustado = int(indice_tarefa) - 1  
-  tarefas[indice_tarefa]["tarefa"] = novo_nome_tarefa
+  tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome_tarefa
   print(f"Tarefa {indice_tarefa} foi atualizada para {novo_nome_tarefa}")
   return
 
