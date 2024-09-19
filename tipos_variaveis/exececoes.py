c66e40b09d0a38dab36a23aca707844cba88e41e
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-19 16:21:38 +0100

    Add files via upload

--- tipos_variaveis/exececoes.py
+++ tipos_variaveis/exececoes.py
@@ -2,6 +2,6 @@ print("As capturas de execessões")
 try:
   numero = input(input("Digite um número: ") )
   resultado = 10 / int(numero)
+  print(f"resultado: {resultado}")
 except Exception as e:
-  print("Erro:", e)
-print(resultado)
+  print("Erro: {e}")
