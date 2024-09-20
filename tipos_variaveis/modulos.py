027fd9d02e90b1836efc8c45e95deeb3ddd27f70
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-09-20 17:45:25 +0100

    Add files via upload

--- tipos_variaveis/modulos.py
+++ tipos_variaveis/modulos.py
@@ -5,7 +5,9 @@ raiz_quadrada = sqrt(16)
 print(raiz_quadrada)
 
 print("\nExemplo de importação de um módulo personalizado:")
-import meu_modulo import saudacao, dobro
+from meu_modulo import saudacao, dobro
 
 mensagem = meu_modulo.saudacao("Mundo")
 resultado_dobro = meu_modulo.dobro(5)
+print(mensagem)
+print(f"O dobro de 5 é {resultado_dobro}")
